dbc58dc14270fcb38d123e72bed192a90ae3d917
DaiYu-233 <103192865+DaiYu-233@users.noreply.github.com>
2022-12-21 11:22:47 +0000

    1 Updated By Github Actions

---
+++ js/totop.js
@@ -0,0 +1,11 @@
+// let qwer = document.getElementById("nav-totop");
+  
+// setInterval("outnum()", 1000)
+
+// function outnum(){
+//     if(result>=99){
+//       qwer.setAttribute("class", "nav-button qazxsw");
+//     }else{
+//       qwer.setAttribute("class", "nav-button");
+//     }
+//   }
